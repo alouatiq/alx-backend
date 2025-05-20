e3c518f31d303b9a708a84f275aff687d4adb4d1
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-05-20 08:45:10 +0200

    Update 0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python3
 """
-Basic Flask app
+Basic Flask app with Babel configuration
 """
 from flask import Flask, render_template
+from flask_babel import Babel
+
+class Config:
+    """Configuration class for Babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
 
 
 @app.route('/')
 def index() -> str:
     """Render the index page"""
-    return render_template('0-index.html')
+    return render_template('1-index.html')
 
 
 if __name__ == '__main__':
