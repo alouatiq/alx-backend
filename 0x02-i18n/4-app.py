28f86c302310acecf4043888ba204b3ed9cd9d51
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-05-20 08:48:22 +0200

    Create 4-app.py

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+Flask app with URL parameter to force locale
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+
+class Config:
+    """Configuration class for Babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Determine the best match locale or use locale URL parameter"""
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index() -> str:
+    """Render the translated index page"""
+    return render_template('4-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
